166e2a9deb24b284513ba3d7b7693a4050cb9f77
Kenan Bouvier <kenanbouvier@gmail.com>
2022-09-17 20:05:44 +0100

    Task1: part 1-3 completed

--- main.go
+++ main.go
@@ -14,14 +14,15 @@ const (
     port     = 5432
     user     = "postgres"
     password = "password"
-    dbname   = "spotlas"
+    dbname   = "postgres"
 )
 
+var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+
+"password=%s dbname=%s sslmode=disable",
+host, port, user, password, dbname);
+var db, err = sql.Open("postgres", psqlInfo);
+
 func main(){
-    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
-    db, err := sql.Open("postgres", psqlInfo)
     if err != nil {
         panic(err)
     }
@@ -31,21 +32,114 @@ func main(){
     if err != nil {
         panic(err)
     }
+    fmt.Println("DB Connection established") ;
 
-    path := filepath.Join("spots.sql");
-    data, ioError := ioutil.ReadFile(path)
-    if ioError != nil {
-        fmt.Println("Error retrieving .sql file: ", ioError);
+
+    // Master switch (var setup) for table initial state setup.
+
+    setup := false;
+
+    if setup{
+        path := filepath.Join("spots.sql");
+        data, ioError := ioutil.ReadFile(path)
+        if ioError != nil {
+            fmt.Println("Error retrieving .sql file: ", ioError);
+        }
+        sql := string(data)
+        _, error := db.Exec(sql);
+
+        if error != nil {
+            fmt.Println("Error executing sql: ",error)
+        }else{
+            fmt.Println("Successfully inserted!")
+        }
     }
-    sql := string(data)
-    _, error := db.Exec(sql);
 
-    if error != nil {
-        fmt.Println("Error executing sql: ",error)
-    }else{
-        fmt.Println("Successfully inserted!")
-    }
 
-    fmt.Println("DB Connection was established")
+    /*  __________________TASK 1 | Query________________________
+
+        Part 1) sql Query that will change the website fields to contain only domain
+
+    */
     
+
+    /* SOLUTION */
+
+    websiteFieldToDomainQuery := `
+        UPDATE "MY_TABLE"
+        SET website = substring(website from '(?:.*://)?(?:www\.)?([^/?]*)')
+    `;
+    executeSQL(websiteFieldToDomainQuery);
+
+
+    
+    /*  __________________TASK 1 | Query________________________
+
+        Part 2) Count how many spots contain the same domain
+
+    */
+    
+    /* SOLUTION */     
+    multipleSpotsCountQuery :=`
+        select COUNT(*) OVER () AS TotalRecords
+        from "MY_TABLE"
+        group by website
+        having count(website)>1
+        limit 1;
+    `;
+    executeSQL(multipleSpotsCountQuery); 
+
+
+
+    /*  __________________TASK 1 | Query________________________
+
+        Part 3) Return spots which have a domain with a count greater than 1
+
+    */
+    
+    /* SOLUTION */     
+    spotsWithMultipleCountQuery :=`
+    SELECT * FROM "MY_TABLE"
+    WHERE website IN (SELECT website
+        FROM "MY_TABLE"
+        GROUP BY website
+        HAVING COUNT(website) > 1 
+    );      
+    `;
+    executeSQL(spotsWithMultipleCountQuery); 
+
+
+
+    /*  __________________TASK 1 | Query________________________
+
+        Part 4) Make a PL/SQL function for point 1.
+        
+    */
+    
+    /* SOLUTION */     
+    // spotsWithMultipleCountQuery :=`
+    // SELECT * FROM "MY_TABLE"
+    // WHERE website IN (SELECT website
+    //     FROM "MY_TABLE"
+    //     GROUP BY website
+    //     HAVING COUNT(website) > 1 
+    // );      
+    // `;
+    // executeSQL(spotsWithMultipleCountQuery); 
+
+
+
 }
+
+func executeSQL(sqlStatement string){
+    _,error := db.Exec(sqlStatement);
+    
+    if error != nil{
+        fmt.Println("Error executing sql Statement: ",error);
+    }else{
+        fmt.Println("Successfully Executed!");
+    }
+}
+
+
+
